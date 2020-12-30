da92d764caca0b1f7ea040178439ad9796ab1192
Forest Katsch <me@forestkatsch.com>
2020-12-29 21:16:22 -0600

    Switch to using Deno hosted version of Apogee

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-#deno run --no-check --unstable --allow-net --allow-read=. --allow-write=dist "https://raw.githubusercontent.com/ForestKatsch/apogee-ssg/main/src/main.ts"
+deno run --no-check --unstable -A "https://deno.land/x/apogee@0.1.0-alpha/main.ts"
+
 #deno run --no-check --unstable --allow-net --allow-read=. --allow-write=dist "../../Software/Apogee/main.ts"
-deno run --no-check --unstable -A "../../Software/Apogee/main.ts"
+#deno run --no-check --unstable -A "../../Software/Apogee/main.ts"
--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 
-export * from '../../Software/Apogee/index.ts';//https://raw.githubusercontent.com/ForestKatsch/apogee-ssg/main/src/index.ts';
+export * from 'https://deno.land/x/apogee@0.1.0-alpha/index.ts';
+//export * from 'https://raw.githubusercontent.com/ForestKatsch/apogee-ssg/main/src/index.ts';
+//export * from '../../Software/Apogee/index.ts';
