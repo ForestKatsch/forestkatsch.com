e7e27fb1d4153ebad7a865e88e2a166837fc5f30
Forest Katsch <me@forestkatsch.com>
2021-03-21 14:34:07 -0500

    Use latest Apogee version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#deno run --no-check --unstable -A "https://deno.land/x/apogee@0.1.1-alpha/main.ts"
+deno run --no-check --unstable -A "https://deno.land/x/apogee@0.1.2-alpha/main.ts"
 #deno run --no-check --unstable -A "../../Software/Apogee/main.ts"
-deno run --unstable -A "../../Software/Apogee/main.ts"
+#deno run --unstable -A "../../Software/Apogee/main.ts"
--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 
-//export * from 'https://deno.land/x/apogee@0.1.1-alpha/mod.ts';
+export * from 'https://deno.land/x/apogee@0.1.2-alpha/mod.ts';
 
-export * from '../../Software/Apogee/mod.ts';
+//export * from '../../Software/Apogee/mod.ts';
